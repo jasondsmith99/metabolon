740320db2bbf936943b475c5d479ebe6ace8c4ab
jasondsmith99 <80532549+jasondsmith99@users.noreply.github.com>
2021-03-19 16:49:38 -0400

    Initial Commit

---
+++ RSSActivityAnalysis/RSSActivityAnalysis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RSSActivityAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        IEnumerable<string> GetCompaniesWithNoActivityFor(IDictionary<string, string> rssUrlDictionary, int numberOfDays)
+        {
+            yield break;
+        }
+    }
+}
